6f8c4a04560ab175c2232b1b0fef6f220562148a
suilahi <suilahizakaria@gmail.com>
2024-12-12 09:30:20 +0100

    modifier la fonction division

--- calcul.java
+++ calcul.java
@@ -8,14 +8,16 @@ public class calcul {
         System.out.println("1 - Addition (+)");
         System.out.println("2 - Soustraction(-)");
         System.out.println("3- Multiplication (*)");
-        System.out.println("3- Division (/)");
-        System.out.println("4 - Quitter");
+        System.out.println("4- Division (/)");
+        System.out.println("5- Puissance");
+        System.out.println("6 - Quitter");
+
         System.out.print("Veuillez entrer votre choix : ");
         Scanner Sc = new Scanner(System.in);
 
             choix = Sc.nextInt();
 
-       if (choix!=5) {
+       if (choix!=6) {
            System.out.println("entrer votre premier nombre:");
            n1 = Sc.nextInt();
            System.out.print("entrer votre deuxieme nombre:");
@@ -35,13 +37,18 @@ public class calcul {
                 resultat=n1 * n2;
                 System.out.println("le resultat est:"+resultat);
             case 4:
-                resultat=n1 /n2 ;
-                System.out.println("le resultat est:"+resultat);
-
+                if (n2!=0) {
+                    resultat= n1 /n2 ;
+                    System.out.println("le resultat est:"+resultat);
+                }else{
+                    System.out.println("erreur,division par 0");
+                }
+                break;
             case 5:
                 resultat = Math.pow(n1, n2);
                 System.out.println("Le résultat de " + n1 + " élevé à la puissance " + n2 + " est : " + resultat);
                 break;
+
             case 6:
                     System.out.println("au revoir!");
                     break;
